573e1c9476784ddabba73fd6a90b64677cd84245
Claudia <claudiasantanagonzalez12@gmail.com>
2026-03-06 21:40:07 +0000

    He creado el menú principal

--- src/app/Main.java
+++ src/app/Main.java
@@ -9,5 +9,16 @@ public class Main {
     public static void main(String[] args) {
         AuthController auth = new AuthController();
         NotaController notas = new NotaController();
+
+        int opcion;
+
+        do {
+            System.out.println("\--- SISTEMA DE NOTAS ---");
+            System.out.println("1. Registrarse");
+            System.out.println("2. Iniciar sesión");
+            System.out.println("0. Salir");
+
+            opcion = InputUtils.leerNumero("Opcion: ");
+        } while (opcion != 0);
     }
 }
