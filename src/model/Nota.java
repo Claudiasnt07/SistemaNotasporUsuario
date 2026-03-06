935b41a3c7d39ac15b1c3ff1f8fc40c75f6cc875
Claudia <claudiasantanagonzalez12@gmail.com>
2026-03-06 20:43:31 +0000

    Cree los métodos get titulo y contenido

--- src/model/Nota.java
+++ src/model/Nota.java
@@ -8,4 +8,12 @@ public class Nota {
         this.titulo = titulo;
         this.contenido = contenido;
     }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
 }
