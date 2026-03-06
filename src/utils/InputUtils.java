300c4cede5ed4ab2428d0b2d6fa064179534e3a4
Claudia <claudiasantanagonzalez12@gmail.com>
2026-03-06 21:24:46 +0000

    He creado el metodo leer numero

--- src/utils/InputUtils.java
+++ src/utils/InputUtils.java
@@ -5,8 +5,13 @@ import java.util.Scanner;
 public class InputUtils {
     private static Scanner scanner = new Scanner(System.in);
 
-    public static String leerTesto(String mensaje) {
+    public static String leerTexto(String mensaje) {
         System.out.print(mensaje);
         return scanner.nextLine();
+    }
+
+    public static int leerTexto(String mensaje) {
+        System.out.print(mensaje);
+        return Integer.parseInt(scanner.nextLine());
     }
 }
