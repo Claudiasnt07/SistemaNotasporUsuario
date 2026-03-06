ce7dffd2427adcaf8707352e219be2465706388e
Claudia <claudiasantanagonzalez12@gmail.com>
2026-03-06 21:05:22 +0000

    Cree el método de crear nota

--- src/controller/NotaController.java
+++ src/controller/NotaController.java
@@ -6,4 +6,8 @@ import java.util.List;
 
 public class NotaController {
     private List<Nota> notas = new ArrayList<>();
+
+    public void crearNota(String email, String titulo, String contenido) {
+        notas.add(new Nota(email, titulo, contenido));
+    }
 }
