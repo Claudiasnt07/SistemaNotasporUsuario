db5f88a8886f57f6c00b94fd23aaa25c909beb6c
Claudia <claudiasantanagonzalez12@gmail.com>
2026-03-06 21:34:11 +0000

    He creado los imports de todo el programa

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,12 @@
 package app;
 
+import controller.AuthController;
+import controller.NotaController;
+import model.Nota;
+import utils.InputUtils;
+
 public class Main {
     public static void main(String[] args) {
-        
+
     }
 }
