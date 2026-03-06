348c08f4fd4ea0a740258318ae07a268abca6e64
Claudia <claudiasantanagonzalez12@gmail.com>
2026-03-06 21:22:52 +0000

    He creado el método leer texto

--- src/utils/InputUtils.java
+++ src/utils/InputUtils.java
@@ -4,4 +4,9 @@ import java.util.Scanner;
 
 public class InputUtils {
     private static Scanner scanner = new Scanner(System.in);
+
+    public static String leerTesto(String mensaje) {
+        System.out.print(mensaje);
+        return scanner.nextLine();
+    }
 }
