94d87bc4283907beacc78b161ae5ce1c9d26d553
Claudia <claudiasantanagonzalez12@gmail.com>
2026-03-06 21:31:23 +0000

    Cree el método hash SHA-256

--- src/utils/SecurityUtils.java
+++ src/utils/SecurityUtils.java
@@ -3,5 +3,20 @@ package utils;
 import java.security.MessageDigest;
 
 public class SecurityUtils {
-    
+    public static String hash(String texto) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] hash = md.digest(texto.getBytes());
+
+            String resultado = " ";
+
+            for (byte b : bytes) {
+                resultado += String.format("%02x", b);
+            }
+
+            return resultado;
+        } catch (Exception e) {
+            return = "";
+        }
+    }
 }
