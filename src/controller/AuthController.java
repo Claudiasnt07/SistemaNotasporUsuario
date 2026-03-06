10c7a457235e8933631073de780a14a766f9ae2c
Claudia <claudiasantanagonzalez12@gmail.com>
2026-03-06 20:59:51 +0000

    Cree el método registrar

--- src/controller/AuthController.java
+++ src/controller/AuthController.java
@@ -6,4 +6,15 @@ import java.util.List;
 
 public class AuthController {
     private List<Usuario> usuarios = new ArrayList<>();
+
+    public boolean registrar(String email, String contraseña) {
+        for (Usuario u : usuarios) {
+            if (u.getEmail().equals(email)) {
+                return false;
+            }
+        }
+
+        usuarios.add(new Usuario(email, contraseña));
+        return true;
+    }
 }
