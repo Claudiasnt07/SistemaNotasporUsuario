cf391a7c1fe0db792c6d1b9812c4521a2a0a5bb5
Claudia <claudiasantanagonzalez12@gmail.com>
2026-03-06 20:42:14 +0000

    Agregué el constructor para que se pueda iniciar tituo y contenido

--- src/model/Nota.java
+++ src/model/Nota.java
@@ -3,4 +3,9 @@ package model;
 public class Nota {
     private String titulo;
     private String contenido;
+
+    public Nota(String titulo, String contenido) {
+        this.titulo = titulo;
+        this.contenido = contenido;
+    }
 }
