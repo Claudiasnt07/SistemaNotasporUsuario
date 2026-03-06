0be8a3cfe1e563248b11119c8f65e947f0fdf6d5
Claudia <claudiasantanagonzalez12@gmail.com>
2026-03-06 21:03:59 +0000

    Cree la lista de notas

---
+++ src/controller/NotaController.java
@@ -0,0 +1,9 @@
+package controller;
+
+import modelo.Nota;
+import java.util.ArrayList;;
+import java.util.List;
+
+public class NotaController {
+    private List<Nota> notas = new ArrayList<>();
+}
