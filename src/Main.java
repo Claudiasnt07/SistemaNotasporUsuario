d2460df18a8d1cf6405fba9b32b01cbee6510826
Claudia <claudiasantanagonzalez12@gmail.com>
2026-06-05 11:30:34 +0100

    Definimos las rutas de almacenamiento y el Scanner para la lectura de datos

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,9 @@ import src.servicio.UsuarioServicio;
 import src.util.HashUtil;
 
 public class Main {
-    
+
+    static Scanner sc = new Scanner(System.in);
+    static Path data = Paths.get("data");
+    static Path usuariosFile = data.resolve("usuarios.txt");
+    static Path usuariosFolder = data.resolve("usuarios");
 }
