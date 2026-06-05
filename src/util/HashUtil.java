eec200848a98451ef659b57706e72adf5a1b7929
Claudia <claudiasantanagonzalez12@gmail.com>
2026-06-05 10:39:16 +0100

    Cree el método estatico para convertir una contraseña en el hash

--- src/util/HashUtil.java
+++ src/util/HashUtil.java
@@ -3,5 +3,18 @@ package src.util;
 import java.security.MessageDigest;
 
 public class HashUtil {
+    public static String hash(String pass) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] bytes = md.digest(pass.getBytes());
+            StringBuilder resultado = new StringBuilder();
+            for (byte b : bytes) {
+                resultado.append(Strin.format("%02x", b));
+            }
+            return resultado.toString();
+        } catch (Exception e) {
+            return pass;
+        }
+    }
     
 }
