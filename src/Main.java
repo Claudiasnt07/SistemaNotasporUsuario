4c99e6b85d191d3acaaf5ca99b82f5b09e77eada
Claudia <claudiasantanagonzalez12@gmail.com>
2026-06-05 11:41:31 +0100

    Crre el método para iniciar el programa

--- src/Main.java
+++ src/Main.java
@@ -12,4 +12,33 @@ public class Main {
     static Path data = Paths.get("data");
     static Path usuariosFile = data.resolve("usuarios.txt");
     static Path usuariosFolder = data.resolve("usuarios");
+
+    public static void main(String[] args) throws Exception {
+        if (!Files.exists(data)) Files.createDirectory(data);
+        if (!Files.exists(usuariosFolder)) Files.createDirectory(usuariosFolder);
+        if (!Files.exists(usuariosFile)) Files.createFile(usuariosFile);
+
+        UsuarioServicio usuarioServicio = new UsuarioServicio(usuariosFile, usuariosFolder);
+
+        int opcion = -1;
+        while (opcion != 0) {
+            System.out.println("\n1 Registrarse\n2 Login\n0 Salir");
+            opcion = Integer.parseInt(sc.nextLine());
+            if (opcion == 1) {
+                System.out.print("Email: ");
+                String email = sc.nextLine();
+                System.out.print("Password: ");
+                String pass = sc.nextLine();
+                usuarioServicio.registrar(email, pass);
+            }
+            if (opcion == 2) {
+                System.out.print("Email: ");
+                String email = sc.nextLine();
+                System.out.print("Password: ");
+                String pass = sc.nextLine();
+                if (usuarioServicio.login(email, pass)) menuNotas(email);
+                else System.out.println("Error");
+            }
+        }
+    }
 }
